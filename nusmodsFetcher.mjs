0205fb5c73b4431a3a125b3a6f2addb6e3095d66
Izen9835 <e1406600@u.nus.edu>
2025-07-12 22:41:59 +0800

    initial commit, awaiting Aayush's backtracking algo

--- nusmodsFetcher.mjs
+++ nusmodsFetcher.mjs
@@ -1,29 +1,71 @@
-// // npm install genetic-js
-// const Genetic = require('genetic-js');
-
-import { Session, Timeslot, Timetable, Module } from './DataStructures.js';
-
 // Purpose: output a list of Modules (as defined in GeneticAlgo.js) 
 // remove other semester's data (e.g. only take data for sem 1)
 // clean any duplicate timeslots (for now just remove duplicates)`
 
-/* what we know so far about duplicate timeslots
-same timing diff loc e..g 16A 16B 16C
+/* 
+
+          "classNo": "B03",
+          "startTime": "1000",
+          "endTime": "1200",
+          "weeks": [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13],
+          "venue": "COM1-B108",
+          "day": "Thursday",
+          "lessonType": "Laboratory",
+          "size": 15,
+          "covidZone": "C"
+
+I have a list of these json objects of the above format
+I need to group them first by lessonType
+ - within lessonType groups, merge items that have the same class number
+ - merge them into the following format:
+        "classNo":"B03",
+        "timing":[
+            [011400, 011600], 
+            [031400, 031600]
+        ],
+        "weeks":[1,2,3,4],
+- where the timings are converted into minutes after Monday 0000hrs, which is calculated using "day" and "startTime"/"endTime"
+
+the final output will be a list of objects, each object having a format like the following
+
+{"module":"CS1010",
+"type":"Laboratory",
+"slots":[
+    {
+        "classNo":"B03",
+        "timing":[
+            [011400, 011600], 
+            [031400, 031600]
+        ],
+        "weeks":[1,2,3,4],
+    },
+    {
+        "classNo":"B07",
+        "timing":[
+            [011400, 011600],
+            [031400, 031600]
+        ],
+        "weeks":[1,2,3,4],
+    },
+]}
+
+for the initial input list of json objects, the "module" is known and is the same for all items
 
 */
 
 
 
 export default class ModFetcher {
-  constructor(modList, sem) {
+  constructor(modList, sem, acadYear) {
     this._modList = modList; // Array of module codes
     this._sem = sem;
-    this.baseUrl = 'https://api.nusmods.com/v2/2024-2025/modules'; //might need to add AY eventually
+    this._acadYear = acadYear;
+    this.baseUrl = 'https://api.nusmods.com/v2/'; //might need to add AY eventually
   }
 
   async fetchSingleModuleData(moduleCode) {
     try {
-      const response = await fetch(`${this.baseUrl}/${moduleCode}.json`);
+      const response = await fetch(`${this.baseUrl}/${this._acadYear}/modules/${moduleCode}.json`);
       if (!response.ok) throw new Error(`HTTP error! status: ${response.status}`);
       return await response.json();
     } catch (error) {
@@ -43,101 +85,128 @@ export default class ModFetcher {
     }
   }
 
+  convertLessons(inputList, moduleCode) {
+    // Mapping from long-form to short-form lesson types
+    const lessonTypeMap = {
+      "Laboratory": "LAB",
+      "Lecture": "LEC",
+      "Tutorial": "TUT",
+      "Recitation": "REC",
+      "Sectional Teaching": "SEC",
+      "Workshop": "WS",
+      "Seminar-Style Module Class": "SEM",
+    };
 
+    const dayToMinutes = {
+      "Monday": 0,
+      "Tuesday": 1 * 24 * 60,
+      "Wednesday": 2 * 24 * 60,
+      "Thursday": 3 * 24 * 60,
+      "Friday": 4 * 24 * 60,
+      "Saturday": 5 * 24 * 60,
+      "Sunday": 6 * 24 * 60
+    };
 
-
-
-  async fetchAllCleanModuleData() {
-    var output = {};
-    var modData = await this.fetchAllModuleData();
-
-    for (var data of modData) {
-      /* check semester availability */
-      var isSemAvail = false;
-      var semData;
-      for (var _semData of data.semesterData) {
-        if (_semData.semester == this._sem) {
-          isSemAvail = true;
-          semData = _semData;
-        }
-      }
-      if (!isSemAvail) throw new Error(`${data.moduleCode} is not available in semester ${this._sem}`)
-      else console.log(`semester data is available for ${data.moduleCode}`);
-
-      console.log(`number of sessions to assign is: ${semData.timetable.length}`);
-
-      output[data.moduleCode] = this.convertToModule(data.moduleCode, semData.timetable, semData.timetable.length);
-
+    function timeToMinutes(timeStr) {
+      const hour = parseInt(timeStr.slice(0, 2), 10);
+      const min = parseInt(timeStr.slice(2), 10);
+      return hour * 60 + min;
     }
 
-    return output;
-  }
-
-
-  convertToModule(moduleCode, semesterData, noToAssign) {
-    /* package semesterData into Timeslots */
-    // each Timeslot EITHER shares the same classNo
-    // OR shares the same timings
-    // not both
-
-    var output = new Module(moduleCode);
-    const assigned = new Set();
-
-    // Step 1: group by classNo
-    const groupByClassNo = Object.groupBy(semesterData,
-      item => item.classNo
-    );
-    for (const group of Object.values(groupByClassNo)) {
-      if (group.length > 1) {
-        var _timeslot = new Timeslot();
-
-        group.forEach(obj => {
-          assigned.add(obj);
-          _timeslot.insertSession(new Session(obj));
-        });
-
-        var activity = group[0].lessonType;
-        output.addTimeslot(activity, _timeslot);
-      }
-    }
-
-    // Step 2: group by Timings
-    const unassigned = semesterData.filter(obj => !assigned.has(obj));
-    const groupByTimings = Object.groupBy(unassigned,
-      item => `${item.startTime}-${item.endTime}-${item.day}`
-    );
-
-    for (const group of Object.values(groupByTimings)) {
-      if (group.length > 1) {
-        var _timeslot = new Timeslot();
-
-        group.forEach(obj => {
-          assigned.add(obj);
-          _timeslot.insertSession(new Session(obj));
-        });
-
-        var activity = group[0].lessonType;
-        output.addTimeslot(activity, _timeslot);
-      }
-    }
-
-    // Singletons (e.g. CS1010 sectional teaching)
-    const remaining = semesterData.filter(obj => !assigned.has(obj));
-    remaining.forEach(obj => {
-      var _timeslot = new Timeslot();
-      _timeslot.insertSession(new Session(obj));
-      output.addTimeslot(obj.lessonType, _timeslot);
+    const grouped = {};
+    inputList.forEach(item => {
+      // Map lessonType to short code, keep original if not mapped
+      const lt = lessonTypeMap[item.lessonType] || item.lessonType;
+      const classNo = item.classNo;
+      const day = item.day;
+      const start = item.startTime;
+      const end = item.endTime;
+      const weeks = item.weeks;
+      const timingObj = {
+        startTime: dayToMinutes[day] + timeToMinutes(start),
+        endTime: dayToMinutes[day] + timeToMinutes(end)
+      };
+      if (!grouped[lt]) grouped[lt] = {};
+      if (!grouped[lt][classNo]) grouped[lt][classNo] = { timing: [], weeks: new Set() };
+      grouped[lt][classNo].timing.push(timingObj);
+      weeks.forEach(w => grouped[lt][classNo].weeks.add(w));
     });
 
-    // console.log("=======================================");
-    // console.log(moduleCode);
-    // console.log(output.availableTimeslots['Laboratory']);
-    // console.log(output.availableTimeslots['Tutorial']);
-    // console.log(output.availableTimeslots['Sectional Teaching']);
-
-    return output;
+    const result = [];
+    for (const [lessonType, classGroups] of Object.entries(grouped)) {
+      const slots = [];
+      for (const [classNo, data] of Object.entries(classGroups)) {
+        slots.push({
+          classNo,
+          timing: data.timing,
+          moduleCode,
+          lessonType, // short code, e.g., "LAB"
+          weeks: Array.from(data.weeks).sort((a, b) => a - b)
+        });
+      }
+      result.push({
+        moduleCode,
+        lessonType, // short code, e.g., "LAB"
+        slots
+      });
+    }
+    return result;
   }
 
+
+
+  getModuleTimetable(modData, moduleCode, semester) {
+    // Find the module by code
+    const moduleObj = modData.find(m => m.moduleCode === moduleCode);
+    if (!moduleObj) throw new Error(`Module ${moduleCode} not found`);
+    // Find the semester data
+    const semObj = moduleObj.semesterData.find(s => s.semester === semester);
+    if (!semObj) throw new Error(`Semester ${semester} not found for module ${moduleCode}`);
+    // Transform timetable
+    return this.convertLessons(semObj.timetable, moduleCode);
+  }
+
+  async getCleaned() {
+    const result = [];
+    const modData = await this.fetchAllModuleData();
+
+    // check availability of modules
+    const missing = this.findUnavailModules(modData, this._modList, this._sem);
+    if (missing.length > 0) {
+      throw new Error(`The following modules are not available: ${missing.join(', ')}`);
+    }
+
+    for (const mod of this._modList) {
+      try {
+        const output = this.getModuleTimetable(modData, mod, this._sem);
+        // console.log(JSON.stringify(output, null, 2));
+        output.forEach(lessonType => result.push(lessonType)); // flatten into one list
+      } catch (e) {
+        console.error(e.message);
+        // Optionally: result.push(null);
+        return [];
+      }
+    }
+
+    return result;
+  }
+
+
+  findUnavailModules(modData, modList, sem) {
+    const missing = [];
+    for (const mod of modList) {
+      const moduleObj = modData.find(m => m.moduleCode === mod);
+      if (!moduleObj) {
+        missing.push(mod);
+        continue;
+      }
+      const semObj = moduleObj.semesterData && moduleObj.semesterData.find(s => s.semester === sem);
+      if (!semObj) {
+        missing.push(mod);
+      }
+    }
+    return missing;
+  }
 
 }
 
--- tests/app.js
+++ tests/app.js
@@ -2,12 +2,28 @@
 import ModFetcher from '../nusmodsFetcher.mjs'
 
 // user input after cleaning
-var _modList = ['CG1111A', 'CS1010'];
+var _modList = ['CG2111A'];
 var _sem = 1
+var _acadYear = "2025-2026";
 
-const fetcher = new ModFetcher(_modList, _sem);
+const fetcher = new ModFetcher(_modList, _sem, _acadYear);
 
-const allModuleCleanData = await fetcher.fetchAllCleanModuleData();
+const allModData = await fetcher.getCleaned();
+
+console.log(allModData);
+
+import { writeFile } from 'fs/promises';
 
 
-// console.log(allModuleCleanData);
+async function main() {
+    const allModData = await fetcher.getCleaned();
+
+    try {
+        await writeFile('cs1010cs1231hsi1000cde2000pf1101.json', JSON.stringify(allModData, null, 2), 'utf-8');
+        console.log('allModData has been saved to allModData.json');
+    } catch (err) {
+        console.error('Error writing file:', err);
+    }
+}
+
+main();
